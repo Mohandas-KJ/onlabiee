dfee9c633600b9c99ec57467c8ea0282ab133310
Mohan Das <mohandaskj.11@gmail.com>
2026-08-25 06:44:29 +0530

    Code on Init Script of onlabiee

--- commands/init.sh
+++ commands/init.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
 init_onlabiee() {
-    echo "Init command used"
+    PROJECT_DIR=$(pwd)
+    PROJECT_NAME="$(basename "$PROJECT_DIR")"
+
+    ONLABIEE_DIR="$PROJECT_DIR/.onlabiee"
+    LOGS_DIR="$ONLABIEE_DIR/logs"
+    CONF_F="$ONLABIEE_DIR/config"
+
+    if [ -d "$ONLABIEE_DIR" ]; then
+         echo "Onlabiee hist repo already exists!"
+         return 1
+    fi
+
+    mkdir -p "$LOGS_DIR"
+
+    echo > "$CONF_F" << EOF
+PROJECT=$PROJECT_NAME
+CREATED=$(date "+%Y-%m-%d %H:%M:%S")
+EOF
+
+    echo "Initialized onlabiee on project: $PROJECT_NAME"
 }
