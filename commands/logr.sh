b54299e7426ce1fb6f8efaa6266cc30f96c85b81
Mohan Das <mohandaskj.11@gmail.com>
2026-09-23 18:30:16 +0530

    enhance: improve logging functionality in start_logr function

--- commands/logr.sh
+++ commands/logr.sh
@@ -2,15 +2,25 @@
 
 start_logr() {
     clear
-    echo "LOG MODE!"
 
     PATH=$(find_project)
 
     status=$?
     if [ $status -eq 0 ]; then
         echo "The Project Dir is: $PATH"
+        LOGS_DIR="$PATH/.onlabiee/logs"
+
+        if [ -d "$LOGS_DIR" ]; then
+            echo "Starting to write..."
+        else 
+            echo "Not Found!"
+            exit 1
+        fi
     else
         echo "Not an onlabiee dir"
+        echo "Run: onlabiee init to add this project!"
         exit 1
     fi
+
+    
 }
